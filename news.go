5ffff2ff45c2708ca074bdfec1610f5e4e39bdfd
Arun Dhyani <arun.dhyani567@gmail.com>
2018-07-21 19:29:56 +0530

    adding template logic to diaply all the news titles and corresponding keywords

--- news.go
+++ news.go
@@ -3,6 +3,7 @@ package main
 import ("fmt" 
 		"net/http"
 		"io/ioutil"
+		"html/template"
 		"encoding/xml")
 
 type Sitemapindex struct {
@@ -15,15 +16,40 @@ type News struct {
 	Locations []string `xml:"url>loc"`
 }
 
-func main() {
+type NewsMap struct {
+	Keyword string
+	Location string
+}
+
+type NewsAggPage struct {
+    Title string
+    News map[string]NewsMap
+}
+
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	resp, _ := 	 http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes,_ := ioutil.ReadAll(resp.Body)
 	var s Sitemapindex
 	xml.Unmarshal(bytes,&s)	
 	var n News
+	news_map := make(map[string]NewsMap)
+
 	for _, Location := range s.Locations {
 		resp, _ := 	 http.Get(Location)
 		bytes,_ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes,&n)
+		for idx, _ := range n.Keywords {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+		fmt.Println(n)
 	}
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+    t, _ := template.ParseFiles("newsaggtemplate.html")
+    t.Execute(w, p)	
+}
+
+func main() {
+	http.HandleFunc("/", newsAggHandler)
+	http.ListenAndServe(":80", nil) 
 }
