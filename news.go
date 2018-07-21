ad6b1676c23a5f8aff7faa9d651ef447055c2682
Arun Dhyani <arun.dhyani567@gmail.com>
2018-07-21 22:11:31 +0530

    adding go concurrency for making faster webserver

--- news.go
+++ news.go
@@ -1,10 +1,14 @@
 package main
 
-import ("fmt" 
+import (//"fmt" 
 		"net/http"
 		"io/ioutil"
 		"html/template"
-		"encoding/xml")
+		"encoding/xml"
+		"sync"
+)
+
+var wg sync.WaitGroup
 
 type Sitemapindex struct {
   Locations []string `xml:"sitemap>loc"`
@@ -26,24 +30,39 @@ type NewsAggPage struct {
     News map[string]NewsMap
 }
 
+func newsRoutine(c chan News,Location string){
+	defer wg.Done()
+	var n News
+	resp, _ := 	 http.Get(Location)
+	bytes,_ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes,&n)
+   	resp.Body.Close()	
+   	c <- n
+}
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	resp, _ := 	 http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes,_ := ioutil.ReadAll(resp.Body)
 	var s Sitemapindex
 	xml.Unmarshal(bytes,&s)	
-	var n News
 	news_map := make(map[string]NewsMap)
+    resp.Body.Close()
+ 	queue := make(chan News, 30)
 
 	for _, Location := range s.Locations {
-		resp, _ := 	 http.Get(Location)
-		bytes,_ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes,&n)
-		for idx, _ := range n.Keywords {
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-		fmt.Println(n)
+		wg.Add(1)
+        go newsRoutine(queue,Location)		
 	}
+
+	wg.Wait()
+    close(queue)
+
+	for elem := range queue {
+        for idx, _ := range elem.Keywords {
+            news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
+        }
+    }
+
 	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
     t, _ := template.ParseFiles("newsaggtemplate.html")
     t.Execute(w, p)	
